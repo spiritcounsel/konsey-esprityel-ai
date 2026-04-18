ad47c1806c753de713729efcf8bf8e343d112630
spiritcounsel <chrislaroc036@yahoo.com>
2026-04-18 16:33:55 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Konsey Espirityèl AI</h1>
-      <p>App is starting...</p>
-    </div>
-  );
-}
