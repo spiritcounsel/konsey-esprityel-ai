b9a11be47e3e21da79d7f188a5d6bd3ae4d8c19e
spiritcounsel <chrislaroc036@yahoo.com>
2026-04-18 16:20:45 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Konsey Espirityèl AI</h1>
+      <p>App is starting...</p>
+    </div>
+  );
+}
